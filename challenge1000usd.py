af6b560f2239d61d62dec754c2b5403cfb353876
Abdoul-le-dev <abdoulledev@gmail.com>
2025-09-10 00:41:12 +0200

    suite

--- challenge1000usd.py
+++ challenge1000usd.py
@@ -63,7 +63,7 @@ L’assistant bot IA du coach Fiacre KPANOU
 
 
 
-async def send_short_link(to_email,update: Update, context: ContextTypes.DEFAULT_TYPE,):
+async def send_short_link(update: Update, context: ContextTypes.DEFAULT_TYPE,):
 
     if not update.message or not update.message.text or "@" not in update.message.text:
         await update.message.reply_text("❌ Merci d’envoyer une adresse email valide.")
