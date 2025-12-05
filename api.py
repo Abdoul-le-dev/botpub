05f9ee0c35fb9a30d20ab087d665da66f6153127
Abdoul-le-dev <abdoulledev@gmail.com>
2025-12-06 00:47:28 +0100

    suite

--- api.py
+++ api.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 app = FastAPI()
 
+origins = [
+    "https://fiacrekpanoutrade.com",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,          # ou ["*"] en dev
+    allow_credentials=True,
+    allow_methods=["*"],            # GET, POST, OPTIONS, etc.
+    allow_headers=["*"],            # Autoriser tous les headers
+)
 
 class RequestBody(BaseModel):
 
