c91d6f6f15a7fdb0df73d90a5ea903ba1fe3322e
Abdoul-le-dev <abdoulledev@gmail.com>
2026-04-18 18:33:17 +0200

    suite

--- api.py
+++ api.py
@@ -5,9 +5,15 @@ from database.database import get_data, get_data_users
 
 app = FastAPI()
 
+origins = [
+    "http://127.0.0.1:8000",
+    "http://localhost:8000",
+]
+
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
