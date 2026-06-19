b956ea07147f5a8c9f892cf94d6218343037bf46
Abdoul-le-dev <abdoulledev@gmail.com>
2026-06-19 10:47:00 +0200

    site

--- form/form_scheduler.py
+++ form/form_scheduler.py
@@ -66,7 +66,7 @@ async def start_scheduler(bot, admin_id: int = None):
         _run_sync_clients_actifs,
         trigger="cron",
         hour=10,
-        minute=46,
+        minute=48,
         id="sync_clients_actifs",
         replace_existing=True,
     )
