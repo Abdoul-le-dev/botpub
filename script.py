4fd93299f09ca3522451fbdb5efec0a4999a8869
Abdoul-le-dev <abdoulledev@gmail.com>
2026-07-27 11:37:17 +0200

    suite

--- script.py
+++ script.py
@@ -37,6 +37,7 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
 )
+logging.getLogger("httpx").setLevel(logging.WARNING)
 logger = logging.getLogger("fdk_bot")
 
 CANAL_B_ID = int(os.getenv("CANAL_B_ID", "-1002705005402"))
