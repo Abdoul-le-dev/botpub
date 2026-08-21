9ee7b52c875af632f21d54523891cecd134112ff
Abdoul-le-dev <abdoulledev@gmail.com>
2026-08-21 10:34:09 +0200

    suite

--- form/form_scheduler.py
+++ form/form_scheduler.py
@@ -65,8 +65,8 @@ async def start_scheduler(bot, admin_id: int = None):
     _scheduler.add_job(
         _run_sync_clients_actifs,
         trigger="cron",
-        hour=12,
-        minute=48,
+        hour=10,
+        minute=36,
         id="sync_clients_actifs",
         replace_existing=True,
     )
